Joker-commits/device_xiaomi_sweet

Product - Board Declaration!
Let the build system to select the right project(s) that best suit our chipset/SoC.

error: external/wpa_supplicant_8/hostapd/Android.mk: error: "hostapd (EXECUTABLES android-arm64) missing lib_driver_cmd_qcwcn (STATIC_LIBRARIES android-arm64)"
You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
external/wpa_supplicant_8/hostapd/Android.mk: error: "hostapd_nohidl (EXECUTABLES android-arm64) missing lib_driver_cmd_qcwcn (STATIC_LIBRARIES android-arm64)"
You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
external/wpa_supplicant_8/wpa_supplicant/Android.mk: error: "wpa_supplicant (EXECUTABLES android-arm64) missing lib_driver_cmd_qcwcn (STATIC_LIBRARIES android-arm64)"
You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
frameworks/opt/net/wifi/libwifi_hal/Android.mk: error: "libwifi-hal (SHARED_LIBRARIES android-arm64) missing libwifi-hal-qcom (STATIC_LIBRARIES android-arm64)"
You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
frameworks/opt/net/wifi/libwifi_hal/Android.mk: error: "libwifi-hal (SHARED_LIBRARIES android-arm64) missing libcld80211 (SHARED_LIBRARIES android-arm64)"
You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
frameworks/opt/net/wifi/libwifi_hal/Android.mk: error: "libwifi-hal (SHARED_LIBRARIES android-arm) missing libwifi-hal-qcom (STATIC_LIBRARIES android-arm)"
You can set ALLOW_MISSING_DEPENDENCIES=true in your environment if this is intentional, but that may defer real problems until later in the build.
frameworks/opt/net/wifi/libwifi_hal/Android.mk

device.mk
@@ -66,3 +66,6 @@ TARGET_DISABLE_MIMOJI_FILES := true
 
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
+
+PRODUCT_BOARD_PLATFORM := sm6150
+PRODUCT_USES_QCOM_HARDWARE := true

OnePlus-Nord4-devs/android_device_oneplus_audi

device.mk
@@ -20,6 +20,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2772
 TARGET_SCREEN_WIDTH := 1240
 
+#Define Board 
+PRODUCT_BOARD_PLATFORM := sm7675
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
@@ -36,6 +40,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm7675-common/common.mk)
+
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/avalon/avalon-vendor.mk)